4f97521e89d680707e9873465890f49c361bf5a9
pellegrineschi <pellegrineschi86@gmail.com>
2023-12-17 13:49:07 -0300

    cards
    aplicando bootstrap, se modifica la funcion mostrarPokemon

--- app.js
+++ app.js
@@ -11,39 +11,76 @@ for (let i = 1; i <= 150; i++) {
 }
 
 //Muestas la data obtenidad del fetch
+// function mostrarPokemon(poke) {
+//   //Me devuelve  un array con los tipos de pokemon y armo con funcion flecha un parrafo con ellos
+//   let tipos = poke.types.map((type) => `<p>tipo: ${type.type.name}</p>`);
+//   //Junta los elementos de un Array en un String
+//   tipos = tipos.join("");
+
+//   const div = document.createElement("div");
+//   div.classList.add("pokemon");
+//   div.innerHTML = `
+  
+  
+    
+//       <div class="col-lg-4 mb-4">
+//           <div class="card">
+//               <img class="card-img-top" src="${poke.sprites.other.dream_world.front_default}" alt="${poke.name}">
+//                   <div class="card-body">
+//                     <h5 class="text-uppercase card-title">${poke.name}</h5>
+//                     <h6 class="card-subtitle text-muted mb-2">#${poke.id}</h6>
+//                      <div class="card" style="width: 18rem;">
+//                         <ul class="list-group list-group-flush">
+//                           <li class="text-uppercase list-group-item">${tipos}</li>
+//                           <li class="list-group-item">Altura: ${poke.height} M</li>
+//                           <li class="list-group-item">Peso: ${poke.weight}KG</li>
+//                         </ul>
+//                       </div>
+//                    <a href="#" class="mt-3 btn btn-primary ">boton</a>
+//                   </div>
+//           </div>
+//       </div>
+    
+//     `;
+
+//   listaPokemon.append(div);
+// }
+
+// en este caso al aplicar bootstarp la proyecto tuvq que modificar la funcion, ya que la misma me crea un div adicional que envuleve a la tajeta
+//al agregar un div adicional por card este me genera conflicto con la grid de bootstarp
+
 function mostrarPokemon(poke) {
-  //Me devuelve  un array con los tipos de pokemon y armo con funcion flecha un parrafo con ellos
+  // Me devuelve un array con los tipos de pokemon y armo con función flecha un párrafo con ellos
   let tipos = poke.types.map((type) => `<p>tipo: ${type.type.name}</p>`);
-  //Junta los elementos de un Array en un String
+  // Junta los elementos de un Array en un String
   tipos = tipos.join("");
 
-  const div = document.createElement("div");
-  div.classList.add("pokemon");
-  div.innerHTML = `
-
-  <div class="pokemon">
-    
-    <div class="pokemon-imagen">
-            <img src="${poke.sprites.other.dream_world.front_default}" alt="${poke.name}">
+  // Crear la tarjeta directamente, sin un div adicional
+  const card = document.createElement("div");
+  card.classList.add("col-lg-4", "mb-4"); // Clases Bootstrap para el diseño
+  card.innerHTML = `
+    <div class="card">
+      <img class="card-img-top" src="${poke.sprites.other.dream_world.front_default}" alt="${poke.name}">
+      <div class="card-body">
+        <h5 class="text-uppercase card-title">${poke.name}</h5>
+        <h6 class="card-subtitle text-muted mb-2">#${poke.id}</h6>
+        <div class="card" style="width: 18rem;">
+          <ul class="list-group list-group-flush">
+            <li class="text-uppercase list-group-item">${tipos}</li>
+            <li class="list-group-item">Altura: ${poke.height} M</li>
+            <li class="list-group-item">Peso: ${poke.weight}KG</li>
+          </ul>
         </div>
-    <div class="pokemon-info">
-        <div class="nombre-contenedor">
-            <p class="pokemon-id">#${poke.id}</p>
-            <h2 class="pokemon-nombre">${poke.name}</h2>
-        </div>
-        <div class="pokemon-tipos">
-         ${tipos}
-        </div>
-        <div class="pokemon-stats">
-            <p>Altura: ${poke.height}M</p>
-            <p>Peso: ${poke.weight}KG</p>
-        </div>
-        
+        <a href="#" class="mt-3 btn btn-primary">boton</a>
+      </div>
     </div>
-</div>`;
+  `;
 
-  listaPokemon.append(div);
+  // Agregar la tarjeta directamente al contenedor de Pokémon
+  listaPokemon.append(card);
 }
+
+
 // botones para filtar por tipos de pokemon
 //Eventos
 botonesHeader.forEach((boton) =>
